586a23e5587e60737407377bbc9282ed3103406c
Kevin Patino <kevindpatino@gmail.com>
2019-05-18 20:47:43 -0500

    Almost 3d :)

--- beamformer_pattern.py
+++ beamformer_pattern.py
@@ -1,58 +1,62 @@
+'''
+=================
+3D wireframe plot
+=================
 
+A very basic demonstration of a wireframe plot.
+'''
+
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+
+import numpy as np
 import math
 import cmath
-import numpy as np
-
-import matplotlib
-import matplotlib.pyplot as plt
 
 sound_speed = 340
 
 n_mics_creator = 8
 n_mics_voice = 7
 
-f=5000
-w = 2*math.pi*f
-
-
 CREATOR = [20.0908795,48.5036755]
-
-size =5000
-vector = np.arange(0,size,1)
-
 CREATOR_radius =  math.sqrt(CREATOR[0]**2 + CREATOR[1]**2)/1000
 print(CREATOR_radius)
 
 n_mics = n_mics_creator
 radius = CREATOR_radius
 
-delta = 2*math.pi*math.sin(math.pi/n_mics)
-
-phi_angle = []
-tau_delay = []
-output = []
-theta = []
-steer_vector = []
+delta = 2*math.pi*math.sin(np.pi/n_mics)
 
 def mag_delay(theta , frequency):
-    for a in vector:
-    angle = (a*360*math.pi)/(len(vector)*180) #((360*a/size-1)*(math.pi/180))
-    theta.append(angle)
-    steer = 0
-    for i in range(0,n_mics):
+  vector = []
+  for f in frequency:
+    acc = []
+    w = 2*math.pi*f
+    for angle in theta:
+      steer = 0
+      for i in range(0,n_mics):
         phi_angle = (2*math.pi*i)/(n_mics)
-        delta_radius = 2*radius*math.sin(math.pi/n_mics)
+        #delta_radius = 2*radius*math.sin(math.pi/n_mics)
         tau_delay =((radius/sound_speed)*math.cos(angle-phi_angle))
         steer += cmath.rect(1,w*tau_delay)
-        
-        #print(abs(steer))
+      db = 20*math.log10(abs(steer)/n_mics)
+      acc.append(db)
+    vector.append(acc)
+  return np.asarray(vector)
 
-    output.append(abs(steer))
+length = 50
+theta = np.linspace(0,2*np.pi,length )
+frequency = np.linspace(8000, 10000, length)
+mag = mag_delay(theta,frequency)
 
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
-ax = plt.subplot(111, projection='polar')
-ax.plot(theta, output)
-ax.grid(True)
+P, F = np.meshgrid(theta,frequency)
+R = mag_delay(theta, frequency)
 
-ax.set_title("A line plot on a polar axis", va='bottom')
+Y, X = R*np.cos(P), R*np.sin(P)
+
+# Plot a basic wireframe.
+ax.plot_surface(X, Y,F, cmap=plt.cm.YlGnBu_r)
 plt.show()
