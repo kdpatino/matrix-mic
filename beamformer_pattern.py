07676ffcbdf7e1293804442981853c6411699350
Kevin Patino <kevindpatino@gmail.com>
2019-05-19 22:43:29 -0500

    Better results

--- beamformer_pattern.py
+++ beamformer_pattern.py
@@ -1,11 +1,3 @@
-'''
-=================
-3D wireframe plot
-=================
-
-A very basic demonstration of a wireframe plot.
-'''
-
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 
@@ -16,14 +8,14 @@ import cmath
 sound_speed = 340
 
 n_mics_creator = 8
-n_mics_voice = 7
+n_mics_voice = 6
 
 CREATOR = [20.0908795,48.5036755]
-CREATOR_radius =  math.sqrt(CREATOR[0]**2 + CREATOR[1]**2)/1000
-print(CREATOR_radius)
+VOICE = [-38.13,3.58]
+BOARD_radius =  math.sqrt(VOICE[0]**2 + VOICE[1]**2)/1000
 
-n_mics = n_mics_creator
-radius = CREATOR_radius
+n_mics = n_mics_voice
+radius = BOARD_radius
 
 delta = 2*math.pi*math.sin(np.pi/n_mics)
 
@@ -39,14 +31,15 @@ def mag_delay(theta , frequency):
         #delta_radius = 2*radius*math.sin(math.pi/n_mics)
         tau_delay =((radius/sound_speed)*math.cos(angle-phi_angle))
         steer += cmath.rect(1,w*tau_delay)
-      db = 20*math.log10(abs(steer)/n_mics)
-      acc.append(db)
+      steer = abs(steer+cmath.rect(1,0))/n_mics+1
+      db = 20*math.log10(steer)
+      acc.append(steer)
     vector.append(acc)
   return np.asarray(vector)
 
-length = 50
+length = 100
 theta = np.linspace(0,2*np.pi,length )
-frequency = np.linspace(8000, 10000, length)
+frequency = np.linspace(10, 10000, length)
 mag = mag_delay(theta,frequency)
 
 fig = plt.figure()
@@ -55,8 +48,10 @@ ax = fig.add_subplot(111, projection='3d')
 P, F = np.meshgrid(theta,frequency)
 R = mag_delay(theta, frequency)
 
-Y, X = R*np.cos(P), R*np.sin(P)
+X, Y = R*np.cos(P), R*np.sin(P)
 
 # Plot a basic wireframe.
-ax.plot_surface(X, Y,F, cmap=plt.cm.YlGnBu_r)
+#ax.contour(X,Y,F,300,extend3d='true')
+ax.plot_surface(X,Y,F,rstride=1, cstride=1,
+                cmap='viridis', edgecolor='none')
 plt.show()
