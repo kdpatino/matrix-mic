98cb5078d09a1759817de0b7d90b135d57a70244
Kevin Patino <kevindpatino@gmail.com>
2019-05-21 16:28:50 -0500

    Add steering vector

--- beamformer_pattern.py
+++ beamformer_pattern.py
@@ -5,6 +5,8 @@ import numpy as np
 import math
 import cmath
 
+fs= 48000
+
 sound_speed = 340
 
 n_mics_creator = 8
@@ -12,32 +14,39 @@ n_mics_voice = 6
 
 CREATOR = [20.0908795,48.5036755]
 VOICE = [-38.13,3.58]
-BOARD_radius =  math.sqrt(VOICE[0]**2 + VOICE[1]**2)/1000
+BOARD = VOICE
+BOARD_radius =  math.sqrt(BOARD[0]**2 + BOARD[1]**2)/1000
 
 n_mics = n_mics_voice
 radius = BOARD_radius
 
 delta = 2*math.pi*math.sin(np.pi/n_mics)
-
+angle_s = 0
 def mag_delay(theta , frequency):
   vector = []
   for f in frequency:
     acc = []
     w = 2*math.pi*f
     for angle in theta:
+      #print("-------------- ",f ,angle*180/math.pi)
       steer = 0
       for i in range(0,n_mics):
-        phi_angle = (2*math.pi*i)/(n_mics)
-        #delta_radius = 2*radius*math.sin(math.pi/n_mics)
+        phi_angle = (2.0*math.pi*i)/(n_mics)
+        delta_radius = 2.0*radius*math.sin(math.pi/n_mics)
         tau_delay =((radius/sound_speed)*math.cos(angle-phi_angle))
-        steer += cmath.rect(1,w*tau_delay)
-      steer = abs(steer+cmath.rect(1,0))/n_mics+1
-      db = 20*math.log10(steer)
-      acc.append(steer)
+        tau_delay_s =((radius/sound_speed)*math.cos(angle_s-phi_angle))
+        #print(tau_delay*fs)
+        #print(cmath.rect(1,w*tau_delay)*cmath.rect(1,w*tau_delay*-1))
+        steer += cmath.rect(1,w*tau_delay)*cmath.rect(1,w*tau_delay_s*-1)
+      steer += cmath.rect(1,0)
+      steer_abs = abs(steer)/float(n_mics+1)
+      print("---------------",steer,steer_abs)
+      db = 20*math.log10(steer_abs)
+      acc.append(steer_abs)
     vector.append(acc)
   return np.asarray(vector)
 
-length = 100
+length = 50
 theta = np.linspace(0,2*np.pi,length )
 frequency = np.linspace(10, 10000, length)
 mag = mag_delay(theta,frequency)
